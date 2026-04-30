856614bde6ba5479c6857504e0de9f0bd6803fa0
unknown <dwikimw666@gmail.com>
2026-04-30 16:37:22 +0700

    Fix Supabase frontend config

--- client/js/supabaseConfig.js
+++ client/js/supabaseConfig.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://pounibztwanrvusjbxzb.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://pounibztwanrvusjbxzb.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvdW5pYnp0d2FucnZ1c2pieHpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MzU4NDgsImV4cCI6MjA5MzExMTg0OH0.mjRL7gx3XJ1erzfyXiDm2mgd8hQH8tNuvqgXckNsbFI";
 
 window.TACTIC_SUPABASE_CONFIG = {
@@ -8,8 +8,8 @@ window.TACTIC_SUPABASE_CONFIG = {
     return Boolean(
       this.url
         && this.anonKey
-        && this.url !== "https://pounibztwanrvusjbxzb.supabase.co/rest/v1/"
-        && this.anonKey !== "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvdW5pYnp0d2FucnZ1c2pieHpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MzU4NDgsImV4cCI6MjA5MzExMTg0OH0.mjRL7gx3XJ1erzfyXiDm2mgd8hQH8tNuvqgXckNsbFI",
+        && this.url !== "YOUR_SUPABASE_URL"
+        && this.anonKey !== "YOUR_SUPABASE_ANON_KEY"
     );
   },
 };
